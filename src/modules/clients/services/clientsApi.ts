084a0baf94688ebdd8f49e281eb6864f241e3e41
YojansC27M <117140677+YojansC27M@users.noreply.github.com>
2026-03-24 13:54:44 -0500

    fix/cli-01
    Se ajustaron errores menores

--- src/modules/clients/services/clientsApi.ts
+++ src/modules/clients/services/clientsApi.ts
@@ -11,17 +11,17 @@ export const getClientById = async (id: string) => {
   return data
 }
 
-export const createClient = async (payload: ClientFormValues) => {
-  const { data } = await api.post<Client>("/clients", payload)
-  return data
+export const createClient = async (data: ClientFormValues) => {
+  const response = await api.post<Client>("/clients", data)
+  return response.data
 }
 
-export const updateClient = async (id: string, payload: ClientFormValues) => {
-  const { data } = await api.put<Client>(`/clients/${id}`)
-  return data
+export const updateClient = async (id: string, data: ClientFormValues) => {
+  const response = await api.put<Client>(`/clients/${id}`, data)
+  return response.data
 }
 
 export const deleteClient = async (id: string) => {
-  const { data } = await api.delete<void>(`/clients/${id}`)
-  return data
+  const response = await api.delete<void>(`/clients/${id}`)
+  return response.data
 }
--- src/modules/clients/types/client.ts
+++ src/modules/clients/types/client.ts
@@ -1,4 +1,4 @@
-export type ClientStatus =  active | suspended | inactive
+export type ClientStatus = 'active' | 'suspended' | 'inactive'
 
 export interface Client {
   id: string
